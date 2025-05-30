6197c00db997e3c1e2fc2d30a241427df51a1e3a
neeraj sachan <neeraj.sachan@sportingtech.com>
2025-05-30 14:24:00 +0530

    remove unwanted lines

--- src/main/java/com/dashboard/app/SonarController.java
+++ src/main/java/com/dashboard/app/SonarController.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dashboard.app.model.Project;
 import com.dashboard.app.service.SonarQubeService;
 
 @RestController
